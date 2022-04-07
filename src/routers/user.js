d8ef908f6522a4edb085a2696e46fd92b9733ecf
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-04-07 23:11:11 +0530

    removed support file

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,7 @@ const express=require('express')
 const User=require('../models/users')
 const router= new express.Router()
 
-router.post('/users', async (req,res)=>{            //creation endpoint  user
+router.post('/users', async (req,res)=>{            //user creation endpoint  
   
     const user=new User(req.body)
    // console.log(req.body)
@@ -16,5 +16,25 @@ router.post('/users', async (req,res)=>{            //creation endpoint  user
         res.status(400).send(error)
     }
 })
+
+router.delete('/users/me',async(req,res)=>{          //user deletion endpoint 
+
+    try{
+        //  const user=await User.findByIdAndDelete(req.user._id)
+
+        //  if(!user)
+        //  {
+        //      return res.status(404).send()
+        //  }
+        //  res.send(user)
+       await req.user.remove()
+        res.send("Deleted")
+    }
+    catch(e)
+    {
+        res.status(500).send(e)
+    }
+})
+
 
 module.exports=router
