b7636b3359c683be8443339504ef02040a5dacf7
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-04-07 23:53:19 +0530

    heroku check

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
-const uri="mongodb+srv://education_insight:Educationinsight@cluster0.5evzv.mongodb.net/education_insight?retryWrites=true&w=majority"
-mongoose.connect(uri,{
+//const uri="mongodb+srv://education_insight:Educationinsight@cluster0.5evzv.mongodb.net/education_insight?retryWrites=true&w=majority"
+mongoose.connect(process.env.MONGODB_URL,{
     
     useNewUrlParser:true,
     useUnifiedTopology:true,
