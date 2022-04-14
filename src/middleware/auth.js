060243ec59769109a3939768a03bb3c8a551cc93
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-04-14 23:29:40 +0530

    EI-61 response messages

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,16 +4,16 @@ const User=require('../models/users')
 const auth= async(req,res,next)=>{
 
     try{
-         console.log('authentication')
+      //   console.log('authentication')
         const token=req.header('Authorization').replace('Bearer ','')
        // console.log(token)
-        console.log(token)
+     //   console.log(token)
         
          const decoded=jwt.verify(token,'thisismygenerator')
          //console.log(decoded)
 
          const user= await User.findOne({_id: decoded._id,'tokens.token':token})
-         console.log('user '+ user)
+       //  console.log('user '+ user)
          if(!user)
          {
              throw new Error
